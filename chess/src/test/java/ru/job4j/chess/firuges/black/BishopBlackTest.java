645dbeee091ec991e1996080d60188504094a557
Ivan <fufyandiya@gmail.com>
2022-02-03 19:38:55 +0300

    2. Каркас шахматной доски [#792]

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -41,30 +41,4 @@ public class BishopBlackTest {
         BishopBlack bishopBlack = new BishopBlack(Cell.D5);
         bishopBlack.way(Cell.E5);
     }
-
-    @Test (expected = OccupiedCellException.class)
-    public void occupiedWay() throws FigureNotFoundException {
-        PawnBlack pawnBlack = new PawnBlack(Cell.E5);
-        BishopBlack bishopBlack = new BishopBlack(Cell.D4);
-        Logic logic = new Logic();
-        logic.add(pawnBlack);
-        logic.add(bishopBlack);
-        logic.move(Cell.D4, Cell.E5);
-    }
-
-    @Test (expected = FigureNotFoundException.class)
-    public void noFigure() throws FigureNotFoundException {
-        BishopBlack bishopBlack = new BishopBlack(Cell.E5);
-        Logic logic = new Logic();
-        logic.add(bishopBlack);
-        logic.move(Cell.D4, Cell.E5);
-    }
-
-    @Test (expected = ImpossibleMoveException.class)
-    public void impossibleMove() throws FigureNotFoundException {
-        BishopBlack bishopBlack = new BishopBlack(Cell.E5);
-        Logic logic = new Logic();
-        logic.add(bishopBlack);
-        logic.move(Cell.E5, Cell.D5);
-    }
 }
